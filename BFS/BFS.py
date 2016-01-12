df16546d0cea66dd0826aa129a66b327a2b4275b
KepenJ <kepenj@gmail.com>
2016-01-12 23:30:06 +0800

    BFS

---
+++ BFS/BFS.py
@@ -0,0 +1,26 @@
+__author__ = 'KepenJ'
+# -*- coding: UTF-8 -*-
+
+'''
+Question : Maze
+
+It represents a labyrinth ,
+in which 1 represents the wall ,
+0 can be the way to go ,
+only to go sideways or vertically to go ,
+can not go sideways ,
+requiring programmed from the upper left to the lower right corner to find the shortest route.
+
+In put:
+
+0 1 0 0 0
+0 1 0 1 0
+0 0 0 0 0
+0 1 1 1 0
+0 0 0 1 0
+
+Out put:
+
+(0, 0),(1, 0),(2, 0),(2, 1),(2, 2),(2, 3),(2, 4),(3, 4),(4, 4)
+
+'''
