26b690cafe412f08a4f9d81fc1e7ba07326ed3e5
KepenJ <kepenj@gmail.com>
2016-01-13 23:44:50 +0800

    BFS_commit

--- BFS/BFS.py
+++ BFS/BFS.py
@@ -13,14 +13,44 @@ requiring programmed from the upper left to the lower right corner to find the s
 
 In put:
 
-0 1 0 0 0
-0 1 0 1 0
-0 0 0 0 0
-0 1 1 1 0
-0 0 0 1 0
+int maze[5][5] = {
+    0, 1, 0, 0, 0,
+    0, 1, 0, 1, 0,
+    0, 0, 0, 0, 0,
+    0, 1, 1, 1, 0,
+    0, 0, 0, 1, 0,
+};
 
 Out put:
 
 (0, 0),(1, 0),(2, 0),(2, 1),(2, 2),(2, 3),(2, 4),(3, 4),(4, 4)
 
 '''
+import struct
+from queue import Queue
+
+class Node:
+    def __init__(self,x=0,y=0):
+        self.x = x
+        self.y = y
+        self.hadVisited = False
+
+def isValid (V):
+    V.x
+
+def BFS(Vs,Ve,allPoint):
+    Q = Queue()
+    Q.put(Vs)
+    visitied = [[]]*5
+    Vn = Vw = Vt = Node()
+    visitied[0][0].append(Vs)
+    Vs.hadVisited = True
+    while Q.empty() != True:
+        Vn = Q.get()
+        for i in range(0:4):
+            Vt = direction[i][0]
+            Vw = Node(Vn.x)
+
+
+
+
