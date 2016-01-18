f7b22c7cdfae8f041ef81ad0cc442cfbe3d2ad1f
KepenJ <kepenj@gmail.com>
2016-01-18 23:43:21 +0800

    update BFS

--- BFS/BFS.py
+++ BFS/BFS.py
@@ -35,13 +35,16 @@ class Node:
         self.hadVisited = False
         self.value = value
     def printSelf(self):
-        return (self.value)
+        if  self.hadVisited == True:
+            return "X"
+        else:
+            return (self.value)
     @classmethod
     def NodeAdd(cls,Va,Vb):
         return Node(Va.x+Vb.x,Va.y+Vb.y,max(Va.value,Vb.value))
 
 def isValid (V):
-    if ((V.x >= 0 and V.x <= 4) and (V.y >= 0 and V.y <= 4)) and V.value == 0:
+    if (V.x >= 0 and V.x <= 4) and (V.y >= 0 and V.y <= 4):
         return True
     else:
         return False
@@ -59,22 +62,32 @@ def BFS(Vs,Ve,outMaze):
     Vs.hadVisited = True
     visitied[0][0] = Vs
     distination = (Node(0,1,0),Node(1,0,0),Node(0,-1,0),Node(-1,0,0))
-    k = 0
     while Q.empty() != True:
         Vn = Q.get()
         for i in range(0,4):
             Vw = Node.NodeAdd(Vn,distination[i])
             if  isEndNote(Vw,Ve):
-                print("Find the way!")
-                print(str(k))
+                print("Find the end point (%d,%d)!"%(Vw.x,Vw.y))
                 return True
             if  isValid(Vw):
-                Vv = visitied[Vw.x][Vw.y]
-                if  Vv.hadVisited != True:
-                    k += 1
-                    Q.put(Vw)
-                    Vw.hadVisited = True
-                    visitied[Vw.x][Vw.y] = Vw
+                Vw.value = visitied[Vw.x][Vw.y].value
+                if Vw.value != 1:
+                    if  Vw.hadVisited != True:
+                        Q.put(Vw)
+                        Vw.hadVisited = True
+                        visitied[Vw.x][Vw.y] = Vw
+
+                        printString = str()
+                        for i in range(0,len(visitied)):
+                            Varray = visitied[i]
+                            for j in range(0,len(Varray)):
+                                V = Varray[j]
+                                printString += str(V.printSelf()) + ' '
+                            printString += '\n'
+                        print("Step:\n")
+                        print(printString)
+
+
     else:
         print("WTF! No result!")
         return False
@@ -105,4 +118,4 @@ def changeIntToNode():
     return outMaze
 
 if  __name__ == '__main__':
-    BFS(Node(0,0),Node(4,2,),changeIntToNode())
+    BFS(Node(0,0),Node(4,4,),changeIntToNode())
