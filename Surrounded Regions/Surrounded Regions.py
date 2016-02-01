6631d92958e4e32af864483052ad57460a7229be
KepenJ <kepenj@gmail.com>
2016-02-01 23:45:28 +0800

    Surrounded Regions

---
+++ Surrounded Regions/Surrounded Regions.py
@@ -0,0 +1,23 @@
+__author__ = 'KepenJ'
+# -*- coding: UTF-8 -*-
+
+'''
+Question:
+
+Given a 2D board containing 'X' and 'O', capture all regions surrounded by 'X'.
+A region is captured by flipping all 'O's into 'X's in that surrounded region .
+
+For example,in put
+X X X X
+X O O X
+X X O X
+X O X X
+
+After running your function, the board should be:
+X X X X
+X X X X
+X X X X
+X O X X
+
+'''
+
