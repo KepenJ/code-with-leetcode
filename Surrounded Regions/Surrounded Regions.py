bb0b3c4aebfe080038e46ee70b1cd0b888420f73
KepenJ <kepenj@gmail.com>
2016-02-02 01:57:34 +0800

    Surrounded Regions. Not yet, have something wrong.

--- Surrounded Regions/Surrounded Regions.py
+++ Surrounded Regions/Surrounded Regions.py
@@ -21,3 +21,66 @@ X O X X
 
 '''
 
+input_map = (
+    ["X","X","X","X","X"],
+    ["X","O","O","X","O"],
+    ["X","X","O","X","X"],
+    ["X","O","X","X","X"],
+    ["X","O","X","X","O"],
+)
+
+box = [[0]*5 for row in range(5)]
+
+def dfs(start_x,start_y):
+    distination = (
+        (0,1),  #right
+        (1,0),  #down
+        (0,-1), #left
+        (-1,0), #up
+    )
+    x = start_x
+    y = start_y
+    for i in range(4):
+        tx = x + distination[i][0]
+        ty = y + distination[i][1]
+        if  tx < 0 or ty < 0 or tx > 4  or ty > 4:
+            continue
+        if  input_map[tx][ty] != 'X' and box[tx][ty] == 0:
+            box[tx][ty] = 1
+            input_map[tx][ty] = 'X'
+            dfs(tx,ty)
+    return
+
+if __name__ == '__main__':
+    print("In put:")
+    print()
+    string = str()
+    for row in input_map:
+        for point in row:
+            string += str(point) + ' '
+        print(string)
+        string = ''
+    print()
+
+    for i in range(5):
+        for j in range(5):
+            if input_map[i][j] != 'X':
+                input_map[i][j] = 'X'
+                box[i][j] = 1
+                dfs(i,j)
+
+    print("Out put:")
+    print()
+    # string = str()
+    # for row in box:
+    #     for num in row:
+    #         string += str(num) + ' '
+    #     print(string)
+    #     string = ''
+    string = str()
+    for row in input_map:
+        for point in row:
+            string += str(point) + ' '
+        print(string)
+        string = ''
+    print()
