e6d948f2872333096e86ddf341d04f7b6279e1b1
KepenJ <kepenj@gmail.com>
2016-02-03 23:44:27 +0800

    although have something problem.

--- Surrounded Regions/Surrounded Regions.py
+++ Surrounded Regions/Surrounded Regions.py
@@ -22,14 +22,13 @@ X O X X
 '''
 
 input_map = (
-    ["X","X","X","X","X"],
-    ["X","O","O","X","O"],
-    ["X","X","O","X","X"],
-    ["X","O","X","X","X"],
-    ["X","O","X","X","O"],
+    ["X","X","X","X"],
+    ["X","O","O","X"],
+    ["X","X","O","X"],
+    ["X","O","X","X"],
 )
 
-box = [[0]*5 for row in range(5)]
+box = [[0]*4 for row in range(4)]
 
 def dfs(start_x,start_y):
     distination = (
@@ -43,12 +42,13 @@ def dfs(start_x,start_y):
     for i in range(4):
         tx = x + distination[i][0]
         ty = y + distination[i][1]
-        if  tx < 0 or ty < 0 or tx > 4  or ty > 4:
+        if  tx < 1 or ty < 1 or tx > 2  or ty > 2:
             continue
         if  input_map[tx][ty] != 'X' and box[tx][ty] == 0:
-            box[tx][ty] = 1
-            input_map[tx][ty] = 'X'
-            dfs(tx,ty)
+            if tx != 0 and ty != 0 and tx != 3 and ty != 3:
+                box[tx][ty] = 1
+                dfs(tx,ty)
+
     return
 
 if __name__ == '__main__':
@@ -62,25 +62,27 @@ if __name__ == '__main__':
         string = ''
     print()
 
-    for i in range(5):
-        for j in range(5):
+    for i in range(4):
+        for j in range(4):
             if input_map[i][j] != 'X':
-                input_map[i][j] = 'X'
                 box[i][j] = 1
                 dfs(i,j)
 
+
+
     print("Out put:")
     print()
-    # string = str()
-    # for row in box:
-    #     for num in row:
-    #         string += str(num) + ' '
-    #     print(string)
-    #     string = ''
     string = str()
-    for row in input_map:
-        for point in row:
-            string += str(point) + ' '
+    for row in box:
+        for num in row:
+            string += str(num) + ' '
         print(string)
         string = ''
-    print()
+
+    # string = str()
+    # for row in input_map:
+    #     for point in row:
+    #         string += str(point) + ' '
+    #     print(string)
+    #     string = ''
+    # print()
