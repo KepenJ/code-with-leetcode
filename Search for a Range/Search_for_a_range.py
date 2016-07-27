543930cd8318a737768a3b15d8690f4362840d4e
Dean Xia <kepenj@gmail.com>
2016-07-27 14:15:12 +0800

    search for a range.

--- Search for a Range/Search_for_a_range.py
+++ Search for a Range/Search_for_a_range.py
@@ -15,8 +15,34 @@ Given [5, 7, 7, 8, 8, 10] and target value 8,
 return [3, 4].
 '''
 
-def search_range(array):
-    pass
+def search_range(array,target):
+    attion_array = [-1,-1]
+    if len(array) <= 1:
+        return attion_array
+    start = 0
+    end = len(array)-1
+    while start <= end:
+        min = start + (end - start)/2
+        if  array[min] >= target:
+            end = min - 1
+        else:
+            start = min + 1
+    if  array[start] == target:
+        attion_array[0]=start
+    else:
+        return attion_array
+
+    end = len(array) -1
+    while start <= end:
+        min = start + (end - start)/2
+        if array[min] <= target:
+            start = min + 1
+        else:
+            end = min - 1
+    if  array[end] == target:
+        attion_array[1] = end
+    return attion_array
 
 if __name__ == '__main__':
-    pass
+    array = [5,7,7,8,8,10]
+    print search_range(array,8)
